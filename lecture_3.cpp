3beb53ae69d3b43afc1db4e790b3f30e4a94604e
CodeWithMU <110249826+CodeWithMU@users.noreply.github.com>
2022-07-29 21:51:44 +0500

    Add files via upload

---
+++ lecture_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,num2;
+    num=3,num2=6;
+    int num3=7;
+    cout<<"the value of num 1 is :"<<num<<endl;
+    cout<<"the value of num 2 is :"<<num2<<endl;
+    cout<<num3;
+    return 0;
+}
